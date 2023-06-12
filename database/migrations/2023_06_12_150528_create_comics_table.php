a6ae1214f620732f94a687b35fce573fc53bddae
marcocnc <marcocancelliere56@gmail.com>
2023-06-12 17:39:03 +0200

    created table

--- database/migrations/2023_06_12_150528_create_comics_table.php
+++ database/migrations/2023_06_12_150528_create_comics_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('thumb');
+            $table->string('price');
+            $table->string('series');
+            $table->string('sale_date');
+            $table->string('type');
+            $table->string('artists');
+            $table->string('writers');
             $table->timestamps();
         });
     }
